bbb75d0f54549386ebe16ab4233103c6a34584de
Macha Priyanka <macha.priyanka@tredence.com>
2024-12-12 18:03:57 +0530

    debug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,8 +13,16 @@ function Home() {
 
     console.log("Custom event triggered with timestamp:", eventTimestamp);
   };
-
   useEffect(() => {
+    if (window.location.hostname != 'localhost') { // Only for local development
+      window.dataLayer = window.dataLayer || [];
+     window.dataLayer.push({
+      'event': 'custom_event',  // Your event name
+      'event_category': 'debug_mode', // Category (optional)
+      'event_timestamp': Date.now(), // Custom parameter for event_timestamp
+      'debug_mode':true,
+    })
+  }
     sendCustomEvent();
   }, []);
 
