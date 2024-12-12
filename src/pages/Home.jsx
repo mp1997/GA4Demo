9c33765b44601d31cae401cd2d217131f2e74b7e
Macha Priyanka <macha.priyanka@tredence.com>
2024-12-12 14:31:31 +0530

    time event

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,24 @@
+import { useEffect } from "react";
 import { Navbar, Main, Product, Footer } from "../components";
 
 function Home() {
+  const sendCustomEvent = () => {
+    const eventTimestamp = Date.now(); // Capture current timestamp
+
+    window.gtag("event", "event_timestamp", {
+      event_timestamp: eventTimestamp,
+      additional_param1: "value1",
+      additional_param2: "value2",
+    });
+
+    console.log("Custom event triggered with timestamp:", eventTimestamp);
+  };
+
+  useEffect(()=>
+    {
+      sendCustomEvent();
+    },[]);
+
   return (
     <>
       <Navbar />
@@ -8,7 +26,7 @@ function Home() {
       <Product />
       <Footer />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
