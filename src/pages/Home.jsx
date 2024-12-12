e9f3997b687a12ee183b1a98d4e141937b702b96
Macha Priyanka <macha.priyanka@tredence.com>
2024-12-12 17:27:55 +0530

    gtag

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,13 @@
 import { useEffect } from "react";
 import { Navbar, Main, Product } from "../components";
+import { gtag } from 'ga-gtag';
 
 function Home() {
   const sendCustomEvent = () => {
     const eventTimestamp = Date.now(); // or a specific timestamp
 
-    window.gtag("event", "custom_event", {
-      event_timestamp: eventTimestamp, // Matches the parameter name in GA4 custom dimension
-      additional_param1: "value1",
-      additional_param2: "value2",
-      debug_mode: true,
+    gtag('event', 'custom_event', {
+      event_timestamp: Date.now(), // Add this custom parameter
     });
 
     console.log("Custom event triggered with timestamp:", eventTimestamp);
