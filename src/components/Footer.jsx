a20dacc2ca0e055777b07a6e2dbd04b03c8c0bd4
Macha Priyanka <macha.priyanka@tredence.com>
2024-12-12 15:53:33 +0530

    time event 2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,6 @@ const Footer = () => {
             <p className="mb-3 mb-md-0">Made with ❤️ by {" "}
               <a  href="https://sahibsingh.dev" className="text-decoration-underline text-dark fs-5" target="_blank" rel="noreferrer">Sahib Singh</a>
             </p>
-            <a className="text-dark fs-4" href="https://github.com/ssahibsingh" target="_blank" rel="noreferrer">
-              <i className="fa fa-github"></i>
-            </a>
           </div>
         </div>
       </footer>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,10 +3,10 @@ import { Navbar, Main, Product, Footer } from "../components";
 
 function Home() {
   const sendCustomEvent = () => {
-    const eventTimestamp = Date.now(); // Capture current timestamp
+    const eventTimestamp = Date.now(); // or a specific timestamp
 
-    window.gtag("event", "event_timestamp", {
-      event_timestamp: eventTimestamp,
+    window.gtag("event", "custom_event", {
+      event_timestamp: eventTimestamp, // Matches the parameter name in GA4 custom dimension
       additional_param1: "value1",
       additional_param2: "value2",
     });
@@ -14,10 +14,9 @@ function Home() {
     console.log("Custom event triggered with timestamp:", eventTimestamp);
   };
 
-  useEffect(()=>
-    {
-      sendCustomEvent();
-    },[]);
+  useEffect(() => {
+    sendCustomEvent();
+  }, []);
 
   return (
     <>
